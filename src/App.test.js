d13aa40671668444771355336b3984398e59c221
KarthikeyaImmaneni <Karthikeya.immaneni@slu.edu>
2025-04-30 03:13:04 +0000

    Karthikeya Immaneni_Week 6

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'tachyons/css/tachyons.min.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
